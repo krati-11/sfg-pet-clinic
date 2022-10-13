45b9cdf0708209ae486b0f9d1563e17980d9fae2
krati-11 <vaishnaok@gmail.com>
2022-10-13 15:12:01 +0530

    added vet list and updated controller. Closed #15

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
